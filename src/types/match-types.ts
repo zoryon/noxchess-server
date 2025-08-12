196870a20b937c6011c37220ba46239be99d5d06
zoryon <gioelespata@gmail.com>
2025-08-12 21:11:56 +0200

    improvements

--- src/types/match-types.ts
+++ src/types/match-types.ts
@@ -1,9 +1,21 @@
+import { JsonValue } from "@prisma/client/runtime/library";
 import { $Enums } from "generated/prisma/index.js";
 
-export type GameState = {
-    match_player: {
+export type GameState = ({
+    match_piece: {
         id: number;
+        status: JsonValue;
+        matchId: number;
+        playerId: number | null;
+        type: $Enums.match_piece_type;
+        posX: number | null;
+        posY: number | null;
+        usedAbility: number | null;
+        captured: number | null;
+    }[];
+    match_player: {
         userId: number | null;
+        id: number;
         matchId: number;
         color: $Enums.match_player_color;
         dreamEnergy: number;
@@ -14,4 +26,4 @@ export type GameState = {
     status: $Enums.match_status;
     winnerId: number | null;
     turn: number;
-}
+}) | null
