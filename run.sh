aa195ac08249471be948a4b2fb5aa3612b4b4aba
zoryon <gioelespata@gmail.com>
2025-08-23 21:49:51 +0200

    updated run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@ set -e
 # Default mode = dev
 MODE=${1:-dev}
 
+
 if [ "$MODE" = "dev" ] || [ "$MODE" = "development" ]; then
-  echo "🚀 Starting container in DEVELOPMENT mode..."
+  echo "🚀 Starting container in DEVELOPMENT mode on port 3001"
   docker run --rm --env-file .env -p 3001:3001 zoryon/noxchess-server:dev
 elif [ "$MODE" = "prod" ] || [ "$MODE" = "production" ]; then
-  echo "🚀 Starting container in PRODUCTION mode..."
+  echo "🚀 Starting container in PRODUCTION mode on port 3001..."
   docker run -d --env-file .env -p 3001:3001 zoryon/noxchess-server:prod
 else
   echo "❌ Unknown mode: $MODE"
